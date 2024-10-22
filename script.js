f142d2a2781135ac3a44082e750e60616ff89e2d
Leonilso <129322347+leonilso@users.noreply.github.com>
2024-10-22 15:19:44 -0300

    colocando dinamização nos vídeos

---
+++ script.js
@@ -0,0 +1,28 @@
+const listaLinks = [
+    "peZa5TKKc4D9ZGpm",
+    "INnXkoJwV0M",
+    "DH72gRefp-E",
+    "C2S1cJIcYus",
+    "gc5MPP3ILHU"
+]
+
+let randomIndex = Math.floor(Math.random() * listaLinks.length);
+let linkEscolhido = arr[randomIndex];
+
+function selecionarVideo(linkEscolhido){
+    let videoPrincipal = document.getElementById("principal")
+    videoPrincipal.src = `https://www.youtube.com/embed/gSWFF1dQTIs?si=${linkEscolhido}`
+
+    let novaLista = listaLinks.slice()
+    let indiceRemover = novaLista.indexOf(linkEscolhido)
+    novaLista.splice(indiceRemover, 1)
+
+
+    for( let i = 0; i < 3; i++){
+        const frame = document.createElement("img");
+        frame.src = `https://img.youtube.com/vi/${novaLista[i]}/maxresdefault.jpg`
+    }
+}
+
+selecionarVideo(linkEscolhido)
+
