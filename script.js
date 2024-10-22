2c3163d9626200439c29840f3d1d9615fb899481
Leonilso <129322347+leonilso@users.noreply.github.com>
2024-10-22 19:07:26 +0000

    Sistema de recomendação confirmada

--- script.js
+++ script.js
@@ -5,23 +5,35 @@ const listaLinks = [
     "C2S1cJIcYus",
     "gc5MPP3ILHU"
 ]
+
+const linkIncorporar = {
+    "gSWFF1dQTIs":"https://www.youtube.com/embed/gSWFF1dQTIs?si=VRhJg9yT7t5pHem20",
+    "INnXkoJwV0M":"https://www.youtube.com/embed/INnXkoJwV0M?si=XnKK9B0EC1zHXmIs",
+    "DH72gRefp-E":"https://www.youtube.com/embed/DH72gRefp-E?si=YCqcnZr6wtqb-EN7",
+    "C2S1cJIcYus":"https://www.youtube.com/embed/C2S1cJIcYus?si=5EQk4Ig8HrJN3Q0o",
+    "gc5MPP3ILHU":"https://www.youtube.com/embed/gc5MPP3ILHU?si=qZsh_jWJYYohRaCS"
+}
+
+
 let randomIndex = Math.floor(Math.random() * listaLinks.length);
 let linkEscolhido = listaLinks[randomIndex];
 
 function selecionarVideo(linkEscolhido){
     let videoPrincipal = document.getElementById("principal")
-    videoPrincipal.src = `https://www.youtube.com/embed/gSWFF1dQTIs?si=${linkEscolhido}`
+    videoPrincipal.src = linkIncorporar[linkEscolhido]
+    console.log(videoPrincipal.src)
 
     let novaLista = listaLinks.slice()
     let indiceRemover = novaLista.indexOf(linkEscolhido)
     novaLista.splice(indiceRemover, 1)
 
     let videosSecundarios = document.getElementById("opcoes")
-    for( let i = 0; i < 3; i++){
+    videosSecundarios.innerHTML = ""
+    for( let i = 0; i < 4; i++){
         const frame = document.createElement("img");
         frame.src = `https://img.youtube.com/vi/${novaLista[i]}/maxresdefault.jpg`
         frame.id = novaLista[i]
-        frame.onclick = selecionarVideo(frame.id)
+        frame.onclick = () => {selecionarVideo(frame.id)}
         videosSecundarios.appendChild(frame)
     }
 }
